51e0e7456165865e91593b2685d7bd5cda643b4d
kunpeisatou <kunpei0411@gmail.com>
2021-10-15 06:43:43 +0000

    Initial Commit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :books
+  get '' => 'homes#top'
+  # get 'edit' => 'books#edit'
+  get 'index' => 'books#index'
+  # get 'show' => 'books#show'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
 end
